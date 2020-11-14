68f4a52665c529b0d37212ac97eeca32d1182c2a
Washi <washinpi@gmail.com>
2020-11-14 18:13:30 +0600

    Day 23 #100-days-of-coding-challenge

---
+++ day-23/sun-of-int.js
@@ -0,0 +1,12 @@
+// Sum of Two Integers
+// https://www.codewars.com/kata/5a9c35e9ba1bb5c54a0001ac
+
+const add = (x, y) => {
+    while (y)  {
+      const carry = x & y
+      x = x ^ y
+      y = carry << 1
+    }
+
+    return x
+}
