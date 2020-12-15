bcc08d6a8f44c2d652bf9db93156489bbbbba97a
Washi <washinpi@gmail.com>
2020-12-15 22:17:49 +0600

    Day 32 #100-days-of-coding-challenge

---
+++ day-32/solve.js
@@ -0,0 +1,10 @@
+// Zalgo text reader
+// https://www.codewars.com/kata/588fe9eaadbbfb44b70001fc
+
+
+function readZalgo(zalgotext) {
+    var mortaltext = zalgotext;
+    return mortaltext.replace( /[^\w.,!? ]/g, "" );
+}
+
+console.log(readZalgo("H̗̪͇͓̙͎̣̄ͬa͚̯̦͉̖̥v͆ͩ̃͆̓̐ͥe̟͎͖͕͍̎ ̰͚̩̟͕̰͊̽̍ͯ̌͊ā̖̪͉͍̥͙̿ͩ̃ͅ ̬̥͎͑̿ͧg̰̳̺͔̦͉ͫ̀̐̓̐r̝̫̱̘̰͐͋ͯͭͭͭ͆e͙͕̖̗͙̰͌ͭä͓͚̝͓́̌͑ͪ͊ṱͥ ̱ͣd͎͔͎͇̫̪̘̃͐̇à͕̮̈͋ͪy̼̳̱ͮ!̳̥̰̭͇̔ͮ̽̓"));
